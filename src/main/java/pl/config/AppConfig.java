fc084f60dadf7418152d985052b92dfd0bac32d0
Hubert Łakomski <hubertlakomski@gmail.com>
2020-04-04 12:13:14 +0200

    Initial commit

---
+++ src/main/java/pl/config/AppConfig.java
@@ -0,0 +1,14 @@
+package pl.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "pl")
+public class AppConfig implements WebMvcConfigurer {
+
+
+}
